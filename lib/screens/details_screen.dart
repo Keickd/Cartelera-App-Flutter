a0d77c97c8f7ec5a635fb74a0c2c4203bc3d9fd9
Keickd <keickd16@gmail.com>
2022-08-15 12:41:42 +0200

    primeros pasos de la pantalla de detalles

--- lib/screens/details_screen.dart
+++ lib/screens/details_screen.dart
@@ -3,9 +3,42 @@ import 'package:flutter/material.dart';
 class DetailsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    //Todo : cambiar por instancia de movie
+
+    final String movie =
+        ModalRoute.of(context)?.settings.arguments.toString() ?? 'no-movie';
+
     return Scaffold(
-      body: Container(
-        child: Center(child: Text('Details Screen')),
+        body: CustomScrollView(
+      slivers: [_CustomAppBar()],
+    ));
+  }
+}
+
+class _CustomAppBar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SliverAppBar(
+      backgroundColor: Colors.indigo,
+      expandedHeight: 200,
+      floating: false,
+      pinned: true,
+      flexibleSpace: FlexibleSpaceBar(
+        centerTitle: true,
+        titlePadding: EdgeInsets.all(0),
+        title: Container(
+            color: Colors.black12,
+            width: double.infinity,
+            alignment: Alignment.bottomCenter,
+            child: const Text(
+              'movie.title',
+              style: TextStyle(fontSize: 16),
+            )),
+        background: const FadeInImage(
+          placeholder: AssetImage('assets/loading.gif'),
+          image: NetworkImage('https://via.placeholder.com/300x400'),
+          fit: BoxFit.cover,
+        ),
       ),
     );
   }
